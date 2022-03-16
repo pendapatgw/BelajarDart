00c28e04193ad14000e39ebdfac6edee1e409cfc
fajar <fajar23.septianto@gmail.com>
2022-03-16 17:14:15 +0700

    update

--- sesi_tiga/dart3(variable).dart
+++ sesi_tiga/dart3(variable).dart
@@ -4,3 +4,5 @@ void main(List<String> args) {
   var umur = '19 tahun';
   print('umur saya adalah $umur');
 }
+// 0-9 adalah bilangan bulat atau integer
+// 0,5 10,0 adalah bilangan desimal atau double
